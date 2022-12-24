54170c14a564b44fdbae53e1e4c40505cf1a8ee6
MMK-13 <kantemamadou13@gmail.com>
2022-12-24 03:21:11 +0000

    Set prod password

--- config/db.php
+++ config/db.php
@@ -1,3 +1,3 @@
 <?php
-	$dao = new PDO('mysql:host=localhost;dbname=games;charset=utf8','root','');
+	$dao = new PDO('mysql:host=localhost;dbname=games;charset=utf8','user','root');
 	$dao->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
